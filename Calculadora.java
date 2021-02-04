a3ee42bbdfc01aa4a217fb94fe66dba99a347845
unclepete-20 <63658079+unclepete-20@users.noreply.github.com>
2021-02-03 23:39:50 -0600

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,8 +1,18 @@
+/**
+ * @author Pedro Arriola
+ * Carnet 20188
+ * Universidad del Valle de Guatemala
+ * Algoritmos y estructuras de datos
+ * Seccion 10
+ */
+
 public interface Calculadora {
+
     /**
-    *
-    * @param expresion
-    * @return calculo realizado por la calculadora.
-    */
-    public String calculo(String expresion);
+     *
+     * @param expresion
+     * @return calculo realizado por la calculadora.
+     */
+    public int calculo(String expresion); //Evalua la expresión que sea ingresada y regresa el resultado.
+
 }
