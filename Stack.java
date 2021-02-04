9735db3f2ff8984a5f38d23c894bb0d5603068dd
unclepete-20 <63658079+unclepete-20@users.noreply.github.com>
2021-02-03 23:43:19 -0600

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,15 +1,24 @@
+/**
+ * @author Pedro Arriola
+ * Carnet 20188
+ * Universidad del Valle de Guatemala
+ * Algoritmos y estructuras de datos
+ * Seccion 10
+ */
+
 public interface Stack<E>{
+
     /**
      *
      * @param item
-     *
+     *Permite ingresar datos al Stack.
      */
     public void push(E item);
 
     /**
      *
      * @return
-     *
+     *Permite sacar datos del Stack.
      */
 
     public E pop();
@@ -17,21 +26,21 @@ public interface Stack<E>{
     /**
      *
      * @return
-     *
+     *Permite obtener el primer dato que aparece sin sacarlo.
      */
     public E peek();
 
     /**
      *
      * @return
-     *
+     *Verifica si el Stack está vacío o no.
      */
     public boolean empty();
 
     /**
      *
      * @return
-     *
+     *Verifica el tamaño del Stack
      */
     public int size();
 }
