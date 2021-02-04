96c1bed3845ed3ec9971f9b0f4c5ecb3efefc730
unclepete-20 <63658079+unclepete-20@users.noreply.github.com>
2021-02-03 18:56:33 -0600

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,3 +1,8 @@
 public interface Calculadora {
-
+    /**
+    *
+    * @param expresion
+    * @return calculo realizado por la calculadora.
+    */
+    public String calculo(String expresion);
 }
