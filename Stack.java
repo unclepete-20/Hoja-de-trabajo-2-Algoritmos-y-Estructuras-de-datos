36ff25625a93e2ddcf3a41bdff5d68e985f12789
unclepete-20 <63658079+unclepete-20@users.noreply.github.com>
2021-02-03 18:57:26 -0600

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,3 +1,37 @@
 public interface Stack<E>{
+    /**
+     *
+     * @param item
+     *
+     */
+    public void push(E item);
 
+    /**
+     *
+     * @return
+     *
+     */
+
+    public E pop();
+
+    /**
+     *
+     * @return
+     *
+     */
+    public E peek();
+
+    /**
+     *
+     * @return
+     *
+     */
+    public boolean empty();
+
+    /**
+     *
+     * @return
+     *
+     */
+    public int size();
 }
