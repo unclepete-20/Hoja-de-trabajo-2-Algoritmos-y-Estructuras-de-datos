6c271a3cafa3d19db5fd59268bcd5fe152984183
unclepete-20 <63658079+unclepete-20@users.noreply.github.com>
2021-02-04 23:34:39 -0600

    Update StackVector.java
    Se agregaron comentarios con formato Javadoc.

--- StackVector.java
+++ StackVector.java
@@ -24,17 +24,36 @@ public class StackVector<E> implements Stack<E> {
         data = new Vector<E>(size);
     }
 
+
+    /**
+     * Pre: Se ingresa dato.
+     * @param item recibe dato para ingresar al stack.
+     * Post: Se almacena en Stack.
+     */
     @Override
     public void push(E item) {
         data.add(item);
     }
 
+
+    /**
+     * Pre:Indice - 1 para el dato en Stack.
+     * @return E cualquier tipo de dato.
+     * Post:Regresa el dato eliminado.
+     */
     @Override
     public E pop() {
 
         return data.remove(size() - 1);
     }
 
+
+    /**
+     * Pre: Revisa el valor mas cercano.
+     * @return E valor deseado.
+     * @throws EmptyStackException
+     * Post: Devuelve el valor inmediato del stack.
+     */
     @Override
     public E peek() throws EmptyStackException {
         if(data.isEmpty()){
@@ -44,12 +63,24 @@ public class StackVector<E> implements Stack<E> {
         return data.elementAt(data.size()-1);
     }
 
+
+    /**
+     * Pre: Verifica si el stack esta vacio.
+     * @return boolean
+     * Post: Regresa falso si no esta vacio, verdadero si esta vacio.
+     */
     @Override
     public boolean empty() {
 
         return data.isEmpty();
     }
 
+
+    /**
+     * Pre: Analiza el stack.
+     * @return int
+     * Post: Devuelve el tamaño del stack.
+     */
     @Override
     public int size() {
 
